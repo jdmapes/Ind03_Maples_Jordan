29dadba0abf79f95399e21ff5088867f005095f0
Jordan Maples <jordan.d.maples@gmail.com>
2022-03-14 21:00:48 -0500

    Initial Commit

---
+++ Ind03_Maples_Jordan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ind03_Maples_Jordan
+//
+//  Created by Jordan Maples on 3/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
