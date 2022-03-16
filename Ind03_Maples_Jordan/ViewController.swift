3b00c19019cac6fdd85c1fcffe73925f8ed02204
Jordan Maples <jordan.d.maples@gmail.com>
2022-03-15 21:56:23 -0500

    Most Recent Changes

--- Ind03_Maples_Jordan/ViewController.swift
+++ Ind03_Maples_Jordan/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stateFlag: UIImageView!
+    @IBOutlet weak var stateShape: UIImageView!
+    @IBOutlet weak var stateName: UILabel!
+    @IBOutlet weak var stateArea: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
